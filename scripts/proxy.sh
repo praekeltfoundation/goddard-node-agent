c22c3def289e4fff4d2932047b6119cf0e1193c7
Johann du Toit <exxonno@gmail.com>
2016-02-25 13:13:37 +0200

    Updated to read the password from the python config file and use that to authenticate to the Mikrotik

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -11,7 +11,7 @@ if [ ! -f "/var/goddard/proxy.lock" ];
   sudo apt-get update
 
   # install squid 
-  sudo apt-get install squid -y
+  sudo apt-get install squid sshpass -y
 
   # update the config
   cp templates/squid.conf /etc/squid3/squid.conf
@@ -30,6 +30,24 @@ if [ ! -f "/var/goddard/proxy.lock" ];
 
   # restart the squid instance to load new config
   service squid3 restart
+
+  # pull out the password for the 750
+  ROUTER_PASSWORD=$(python -c "import imp; localsettings=imp.load_source('localsettings', '/var/goddard/node_updater/local_settings.py'); print localsettings.NEW_RB750_PASSWORD")
+
+  # debug
+  echo "Found Password: $ROUTER_PASSWORD" 
+
+  # run the commands to migrate changes over to mikrotik
+  sshpass -p "$ROUTER_PASSWORD" ssh admin@192.168.88.5 "$(which bash) -s" << EOF
+      /ip hotspot user profile set 0 transparent-proxy=yes
+      /ip proxy set enabled=yes
+      /ip proxy set parent-proxy=192.168.88.50 set parent-proxy-port=3128
+      /ip hotspot walled-garden remove numbers=[/ip hotspot walled-garden find ]
+      /ip hotspot walled-garden add action=deny dst-host=192.168.88.5 server=hotspot1
+      /ip hotspot walled-garden add action=deny dst-host=192.168.88.10 server=hotspot1
+      /ip hotspot walled-garden add action=deny dst-host=192.168.88.50 server=hotspot1
+      /ip hotspot walled-garden add action=allow dst-host=* server=hotspot1
+  EOF
 
   # write our log file
   date > /var/goddard/proxy.lock
