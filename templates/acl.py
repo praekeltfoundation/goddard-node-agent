50d616574e7daac9deb5b4c1ae45d9408f408e82
Johann du Toit <exxonno@gmail.com>
2016-02-25 10:47:07 +0200

    Updated ACL.py to actually read in whitelist from node

--- templates/acl.py
+++ templates/acl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import io
 import sys
+import json
 
 ###
 # cached version of the website
@@ -15,16 +16,59 @@ while True:
 
 
   if line:
+
+    """
     # debugging
     f = open('/var/goddard/acl.txt', 'a')
     f.write( str(line) + '\n' )
     f.close()
+    """
 
     # get the whitelisted domains
     if len(cached_whitelist) == 0:
 
-      # load in the whitelist
-      cached_whitelist = [ 'opera', 'io.co.za', 'reddit.com', 'captive.apple.com', 'clientconnectivitycheck.android.com', 'clients3.google.com' ]
+      # awesome so read in the whitelist with a few of our own defaults as well
+      cached_whitelist = [
+
+        'captive.apple.com', 
+        'clientconnectivitycheck.android.com', 
+        'clients3.google.com'
+
+      ]
+
+      # get the node info
+      node_info_obj = None 
+
+      # try to parse it out
+      try:
+
+        # try to read as a file
+        f = open('/var/goddard/node.json', 'r')
+
+        # get the contents
+        file_content_str = str(f.read())
+
+        # parse as JSON object
+        node_info_obj = json.loads( file_content_str )
+
+        # close the handler
+        f.close()
+
+      except Exception, e: pass
+
+      # if we found the node object
+      if node_info_obj == None:
+
+        # set to empty again to retry the next time
+        cached_whitelist = []
+
+      else:
+
+        # add all the domains
+        for whitelist_obj in node_info_obj['whitelist']:
+
+          # add the domain
+          cached_whitelist.append( whitelist_obj['domain'] )
 
     # output to send out to Squid
     filter_output_str = 'ERR'
